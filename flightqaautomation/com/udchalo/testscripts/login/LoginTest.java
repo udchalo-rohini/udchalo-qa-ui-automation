43d73d1410a467b4f5aeedea6e173c11984b3c7e
Rohini Kulkarni <rohini.kulkarni@udchalo.com>
2019-07-25 17:23:30 +0530

    LoginTestClass updated

--- flightqaautomation/com/udchalo/testscripts/login/LoginTest.java
+++ flightqaautomation/com/udchalo/testscripts/login/LoginTest.java
@@ -1,6 +1,28 @@
+import org.testng.Assert;
 
 public class LoginTest extends TestBase{
+
+private LoginLogoutView objLoginLogoutView;
+
+	
+	@BeforeClass
+	public void initilizeWebEnvironment(){
+		objLoginLogoutView=new LoginLogoutView(this);
+		this.initilize_Properties_And_Web_Environment();
+	}
+	
+	@Test
+	public TC001_LoginTest(){
+		Assert.assertTrue(true);
+	}
 	
 	
+	@AfterClass
+	public void tearDownEnvironment(){
+		
+		this.tearDown_Web_Environment();
+		
+	}
+
 
 }
